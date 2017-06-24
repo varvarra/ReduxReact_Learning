b645dcd3c9940ae7ef31a30c985e4b37aa1945c1
Barbara Shinkarenko <shinkarenko.varvara@gmail.com>
2017-06-24 15:08:51 +0100

    Initial Setup

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
+const API_KEY = 'AIzaSyDOZ6hJVIIqJHUn08fcARdBu68XKKdtIW0';
+
+
+
 import App from './components/app';
 import reducers from './reducers';
 
